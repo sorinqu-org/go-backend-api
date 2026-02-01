bb98c60a5528cac0208a1edb8db3662c19350da0
Sorinqu <artem.math.bg@gmail.com>
2026-02-01 22:23:17 +0500

    Base API Structure

--- cmd/api.go
+++ cmd/api.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(time.Minute))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("This is the API root\n"))
+	})
+
+	log.Printf("Server started at addr: %v", app.config.addr)
+	fmt.Printf(`
+  _____       ___        _ 
+ / ___/__    / _ | ___  (_)
+/ (_ / _ \  / __ |/ _ \/ / 
+\___/\___/ /_/ |_/ .__/_/  
+                /_/
+
+`)
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:    app.config.addr,
+		Handler: h,
+	}
+
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string // database url
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+)
+
 func main() {
+	cfg := config{
+		addr: ":3333",
+		db:   dbConfig{},
+	}
+
+	api := application{
+		config: cfg,
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := api.run(api.mount()); err != nil {
+		slog.Error("Failed to start", "error", err)
+		os.Exit(1)
+	}
 }
